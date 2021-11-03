ab2ed18b2489f669b73d06b9056c211f90b51c33
Michał Płużek <michalpluzek@gmail.com>
2021-11-03 15:31:34 +0100

    Add form and simply form validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { SignupFormComponent } from "./signup-form.component";
 
 @NgModule({
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   declarations: [AppComponent, SignupFormComponent],
   providers: [],
   bootstrap: [AppComponent],
--- src/signup-form.component.ts
+++ src/signup-form.component.ts
@@ -2,6 +2,26 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "signup-form",
-  template: ` do zrobienia...`,
+  template: `
+    <form (submit)="onSubmit()">
+      <div class="mb-3">
+        <label class="form-label"> Email </label>
+        <input
+          type="email"
+          class="form-control"
+          [(ngModel)]="email"
+          name="email"
+          required
+        />
+      </div>
+      <button class="btn btn-primary" type="submit">Rejestracja</button>
+    </form>
+  `,
 })
-export class SignupFormComponent {}
+export class SignupFormComponent {
+  email: string = "";
+
+  onSubmit() {
+    console.log("Test onSubmit", this.email);
+  }
+}
